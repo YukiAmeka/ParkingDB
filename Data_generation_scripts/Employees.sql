c6dce38693f4611ad60f7759e093ba701c7df804
Mariia-ter <62756786+Mariia-ter@users.noreply.github.com>
2020-04-14 12:13:51 +0300

    Employees.sql
    Added generation script for Employees table

---
+++ Data_generation_scripts/Employees.sql
@@ -0,0 +1,23 @@
+--generating data to Employee Table---------
+INSERT INTO [Staff].[Employees2] (FirstName, Surname, Gender, DateOfBirth, PhoneNumber, Email, CityID, HomeAddress, LotID, DateHired, PositionID, Salary, ManagerID, DateFired) 
+VALUES 
+('Jacob', 'Burnett', 'm',	'1977-06-02',	'02032512556',	'jbu3@parkinglot.com', '1', '1970 Napa Ct.', '1', '1254', '1', '1700', '7', '1812'),
+('Griffith', 'Dalton', 'm',	'1961-11-18',	'02033655479',	'gda2@parkinglot.com', '1',	'1226 Shoe St.', '1', '1123',	'1', '1900', '8', '1812')
+
+INSERT INTO [Staff].[Employees2] (FirstName, Surname, Gender, DateOfBirth, PhoneNumber, Email, CityID, HomeAddress, LotID, DateHired, PositionID, Salary, ManagerID) 
+VALUES 
+('Lionel', 'Key', 'f',	'1971-04-20',	'02032517556',	'lke2@parkinglot.com', '1', '9833 Mt. Dias Blv.', '1',	'1828', '1', '1700', '6'),
+('Elmo', 'Chapmant', 'm',	'1987-01-02',	'02032556355',	'ech4@parkinglot.com', '1',	'7484 Roundtree Drive',	'1',	'1828', '1', '1700', '6'),
+('Mari', 'Carpenter', 'm',	'1967-04-05',	'02030002586',	'mca4@parkinglot.com', '1',	'9539 Glenside Dr',	'1',	'1828',	'1', '1700', '6'),
+('Chiquita', 'Taylor', 'f',	'1980-02-18',	'02031125587',	'cta3@parkinglot.com', '1',	'1399 Firestone Drive', '1', '1828', '2', '1900', '7'),
+('Dominique', 'Burns', 'f',	'1977-04-23',	'02039699666',	'dbu1@parkinglot.com', '1',	'5672 Hale Dr.', '1', '1812',	'3', '5000', '8'),
+('Peter', 'Curtis', 'm',	'1967-01-14',	'02032559999',	'pcu4@parkinglot.com', '1',	'6387 Scenic Avenue', '1', '1823', '4', '8000', '14'),
+('Kevyn', 'Vang', 'm',	'1965-08-10',	'02034478963',	'kva4@parkinglot.com', '1',	'8713 Yosemite Ct.', '1', '1809', '6', '3500', '10'),
+('Jeremy', 'Hopking', 'm',	'1965-08-10',	'02034478963',	'jho4@parkinglot.com', '1',	'220-9050 Quis Street', '1', '1788',	'7', '5500', '14'),
+('Lacota','Hull', 'm',	'1986-05-12',	'02034478963',	'lhu4@parkinglot.com', '1',	'483-3758 Curabitur Avenue', '1', '1756',	'8', '6500', '12'),
+('Azalia','Ratliff', 'f',	'1974-11-29',	'02034478963',	'ara4@parkinglot.com', '1',	'Ap #715-3777 Eu Avenue', '1', '1803',	'9', '8000', '14'),
+('Davis','Livingston', 'm',	'1970-09-24',	'02034478963',	'dli4@parkinglot.com', '1',	'1008 Ipsum. St.', '1', '1816',	'10', '5000', '14')
+
+INSERT INTO [Staff].[Employees2] (FirstName,Surname, Gender, DateOfBirth, PhoneNumber, Email, CityID, HomeAddress, LotID, DateHired, PositionID, Salary) 
+VALUES 
+('Jameson', 'Fulton', 'm',	'1965-10-27',	'02038878964',	'jfu6@parkinglot.com', '1',	'250 Race Court', '1', '1722', '5', '10000')
