a2db3bfb157e1aec3d9351f41d12adde53d0ee73
SerhiiKopytko <63491903+SerhiiKopytko@users.noreply.github.com>
2020-04-14 12:32:08 +0300

    Parking.ZoneTypes.sql
    Added generation scripts for Parking.ZoneTypes

---
+++ Data_generation_scripts/Parking.ZoneTypes.sql
@@ -0,0 +1,6 @@
+INSERT INTO Parking.ZoneTypes (ZoneTypeName, Description, SlotSizeID)
+VALUES 
+('A', 'Standard slot for owners of membership', 1),
+('B', 'Extended slot for owners of membership', 2),
+('C', 'Standard slot for walk-in clients', 1),
+('D', 'Extended slot for walk-in clients', 2)
