ee5f5a26f05f9abf928bb60c237d6f69c491e93a
vlavryntsiv <63586235+vlavryntsiv@users.noreply.github.com>
2020-04-21 23:20:46 +0300

    Update TariffNames.sql

--- Data_generation_scripts/TariffNames.sql
+++ Data_generation_scripts/TariffNames.sql
@@ -1,6 +1,6 @@
 INSERT INTO Operation.TariffNames (Name, Description) VALUES
   ('DayHour', 'Price for 1 hour  from 6 a.m. to 6 p.m.'),
   ('NightHour', 'Price for 1 hour from 6 p.m. to 6 a.m.'),
-  ('DayShift', 'Price for 12 hours from 6 p.m. to 6 a.m.'),
+  ('DayShift', 'Price for 12 hours from 6 a.m. to 6 p.m.'),
   ('NightShift', 'Price for 12 hours from 6 p.m. to 6 a.m.'),
  	('AllDay', 'Price for 24 hours')
