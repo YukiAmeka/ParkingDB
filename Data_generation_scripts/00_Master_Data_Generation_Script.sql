3e5a804998b58192a8eeaefa10a407919830bceb
StasGit <kutsyukk@gmail.com>
2020-04-26 22:01:48 +0300

    Renamed STP_InsertDataSlots to /STP_GNR_ParkingSlots

--- Data_generation_scripts/00_Master_Data_Generation_Script.sql
+++ Data_generation_scripts/00_Master_Data_Generation_Script.sql
@@ -17,6 +17,7 @@ EXEC STP_GNR_ParkingSlotSizes;   --Parking.SlotSizes.sql
 EXEC STP_GNR_ParkingZoneTypes;    --Parking.ZoneTypes.sql
 EXEC STP_GNR_ParkingLots;         --Parking.Lots.sql
 EXEC STP_GNR_ParkingZones;       --Parking.Zones.sql
+EXEC STP_GNR_ParkingSlots
 EXEC STP_GNR_OperationTariffNames;
 EXEC STP_GNR_StaffPositions;   --StaffPositions.txt 
 EXEC STP_GNR_CalendarData;
