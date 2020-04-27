afd8559db02dfe49a33629142c661f276b1f5fb4
YukiAme <anyalo307@gmail.com>
2020-04-27 13:56:10 +0300

    Order of presentation

--- Data_generation_scripts/00_Master_Data_Generation_Script.sql
+++ Data_generation_scripts/00_Master_Data_Generation_Script.sql
@@ -2,12 +2,12 @@
 /* CREATE FIRST. DO NOT EXECUTE */
 
 -- STP_HLP_OldOperationTariffs
--- STP_HLP_GenerateRandomTime - Generate random time within given limits
--- STP_HLP_FindEmployeeOnShift - Find the Employee who is/was on shift at a given Parking Lot at a given date & time
--- STP_HLP_OldMembershipTariffs
--- STP_HLP_PurchaseDateForActiveCard - Generate random purchase date & calculate expiry date for a card that is active at a given date
--- STP_HLP_AddMembershipPurchase - Insert 1 record about a membership card purchase
--- STP_HLP_ClientsExpiredMembership - Generate historical log of membership cards' purchases for 1 client
+-- STP_HLP_GenerateRandomTime - Ігор - Generate random time within given limits
+-- STP_HLP_FindEmployeeOnShift - Аня - Find the Employee who is/was on shift at a given Parking Lot at a given date & time
+-- STP_HLP_OldMembershipTariffs - Ігор
+-- STP_HLP_PurchaseDateForActiveCard - Ваня - Generate random purchase date & calculate expiry date for a card that is active at a given date
+-- STP_HLP_AddMembershipPurchase -  Аня  - Insert 1 record about a membership card purchase
+-- STP_HLP_ClientsExpiredMembership -  Аня  - Generate historical log of membership cards' purchases for 1 client
 -- STP_HLP_DateTime - Generate random date and time for operation orders
 
 
@@ -20,34 +20,34 @@
 /* EXECUTE IN THIS ORDER */
 
 EXEC STP_GNR_CalendarData;
-EXEC STP_GNR_LocationCities;
+EXEC STP_GNR_LocationCities; -- прост
 EXEC STP_GNR_ParkingLots;
 EXEC STP_GNR_StaffPositions;
 EXEC STP_GNR_EmployeesReserve
-EXEC STP_GNR_CurrentEmployees   
+EXEC STP_GNR_CurrentEmployees
 EXEC STP_GNR_FiredAttendants
 EXEC STP_GNR_PositionChanges
 EXEC STP_GNR_StaffShifts '2020-04-30'
 EXEC STP_GNR_SalaryChanges
-EXEC STP_GNR_ParkingSlotSizes;  
-EXEC STP_GNR_ParkingZoneTypes;  
-EXEC STP_GNR_ParkingZones;  
+EXEC STP_GNR_ParkingSlotSizes;
+EXEC STP_GNR_ParkingZoneTypes;
+EXEC STP_GNR_ParkingZones;
 EXEC STP_GNR_ParkingSlots;
 EXEC STP_GNR_OperationTariffNames
 EXEC STP_GNR_OperationTariffs
-EXEC STP_GNR_MembershipPeriods;
-EXEC STP_GNR_MembershipTariffs;
-EXEC STP_GNR_ClienteleClients; 						     
+EXEC STP_GNR_MembershipPeriods; -- прост
+EXEC STP_GNR_MembershipTariffs; -- Ігор
+EXEC STP_GNR_ClienteleClients; -- Аня
 EXEC STP_GNR_ClienteleCars;
-EXEC STP_GNR_MembershipAllCards;
-EXEC STP_GNR_MembershipOrders;
-EXEC STP_GNR_MembershipActiveCards
+EXEC STP_GNR_MembershipAllCards; -- Ваня
+EXEC STP_GNR_MembershipOrders; -- Аня
+EXEC STP_GNR_MembershipActiveCards -- Ігор
 EXEC STP_GNR_OperationOrdersUnregularClients
 EXEC STP_GNR_OperationOrdersUnregularRestClients
 EXEC STP_GNR_OperationOrdersMembers
 
 
-   
+
 
 
 
