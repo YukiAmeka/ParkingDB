87bc19f6eed7cb8cbf41d676f81211b71e723847
YukiAme <anyalo307@gmail.com>
2020-04-27 16:01:37 +0300

    Order of presentations

--- Data_generation_scripts/00_Master_Data_Generation_Script.sql
+++ Data_generation_scripts/00_Master_Data_Generation_Script.sql
@@ -2,12 +2,12 @@
 /* CREATE FIRST. DO NOT EXECUTE */
 
 -- STP_HLP_OldOperationTariffs
--- STP_HLP_GenerateRandomTime - Generate random time within given limits
--- STP_HLP_FindEmployeeOnShift - Find the Employee who is/was on shift at a given Parking Lot at a given date & time
--- STP_HLP_OldMembershipTariffs
--- STP_HLP_PurchaseDateForActiveCard - Generate random purchase date & calculate expiry date for a card that is active at a given date
--- STP_HLP_AddMembershipPurchase - Insert 1 record about a membership card purchase
--- STP_HLP_ClientsExpiredMembership - Generate historical log of membership cards' purchases for 1 client
+-- STP_HLP_GenerateRandomTime - Ihor - Generate random time within given limits
+-- STP_HLP_FindEmployeeOnShift - Anya (?) - Find the Employee who is/was on shift at a given Parking Lot at a given date & time
+-- STP_HLP_OldMembershipTariffs - Ihor
+-- STP_HLP_PurchaseDateForActiveCard - Vanya - Generate random purchase date & calculate expiry date for a card that is active at a given date
+-- STP_HLP_AddMembershipPurchase - Anya - Insert 1 record about a membership card purchase
+-- STP_HLP_ClientsExpiredMembership - Anya - Generate historical log of membership cards' purchases for 1 client
 -- STP_HLP_DateTime - Generate random date and time for operation orders  --Volodymyr
 
 
@@ -22,31 +22,31 @@
 
 
 EXEC STP_GNR_CalendarData; -- Stas
-EXEC STP_GNR_LocationCities; 
-EXEC STP_GNR_ParkingLots; 
+EXEC STP_GNR_LocationCities;
+EXEC STP_GNR_ParkingLots;
 EXEC STP_GNR_StaffPositions;
-EXEC STP_GNR_EmployeesReserve   --Марія
-EXEC STP_GNR_CurrentEmployees   -- Марія
-EXEC STP_GNR_FiredAttendants    -- Марія
+EXEC STP_GNR_EmployeesReserve   --пїЅпїЅпїЅпїЅ
+EXEC STP_GNR_CurrentEmployees   -- пїЅпїЅпїЅпїЅ
+EXEC STP_GNR_FiredAttendants    -- пїЅпїЅпїЅпїЅ
 EXEC STP_GNR_PositionChanges    -- Sergiy
-EXEC STP_GNR_StaffShifts '2020-04-30' -- Марія
+EXEC STP_GNR_StaffShifts '2020-04-30' -- пїЅпїЅпїЅпїЅ
 EXEC STP_GNR_SalaryChanges		-- Sergiy
-EXEC STP_GNR_ParkingSlotSizes;  
-EXEC STP_GNR_ParkingZoneTypes;  
-EXEC STP_GNR_ParkingZones;  
+EXEC STP_GNR_ParkingSlotSizes;
+EXEC STP_GNR_ParkingZoneTypes;
+EXEC STP_GNR_ParkingZones;
 EXEC STP_GNR_ParkingSlots;          -- Volodymyr
 EXEC STP_GNR_OperationTariffNames
 EXEC STP_GNR_OperationTariffs       --Volodymyr
 EXEC STP_GNR_MembershipPeriods;
-EXEC STP_GNR_MembershipTariffs;
-EXEC STP_GNR_ClienteleClients; 						     
+EXEC STP_GNR_MembershipTariffs; -- Ihor
+EXEC STP_GNR_ClienteleClients; 	-- Anya
 EXEC STP_GNR_ClienteleCars;   -- Volodymyr
-EXEC STP_GNR_MembershipAllCards;
-EXEC STP_GNR_MembershipOrders;  -- Аnna
-EXEC STP_GNR_MembershipActiveCards  --
+EXEC STP_GNR_MembershipAllCards; -- Vanya
+EXEC STP_GNR_MembershipOrders;  -- Anya
+EXEC STP_GNR_MembershipActiveCards  -- Ihor
 EXEC STP_GNR_OperationOrdersUnregularClients  --  Stas
 EXEC STP_GNR_OperationOrdersUnregularRestClients  -- Stas
-EXEC STP_GNR_OperationOrdersMembers -- Sergiy\Anna
+EXEC STP_GNR_OperationOrdersMembers -- Sergiy
 
 
 
