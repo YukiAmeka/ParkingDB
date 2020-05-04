f7f314f4fd51a65bf072ca2a39c9508659d659d6
Mariia-ter <terletska.mariia@gmail.com>
2020-05-04 23:28:49 +0300

    Added VW_STG_OperationDimTariffs

---
+++ Staging_DataMart/VW_STG_OperationDimTariffs.sql
@@ -0,0 +1,27 @@
+/* Create OLTP view for future data transfer into Data Mart Operation dimension Tariffs */
+
+CREATE VIEW VW_STG_OperationDimTariffs
+AS
+SELECT t.TariffID
+	, tn.Name AS TariffName
+	, c.CityName AS City
+	, l.LotName AS ParkingName
+	, zt.ZoneTypeName AS ZoneType
+	, s.SlotDescription AS SlotType
+	, t.Price
+	, cd1.TheDate AS TariffStartDate
+	, cd2.TheDate AS TariffEndDate
+
+FROM Operation.Tariffs AS t
+INNER JOIN Operation.TariffNames AS tn ON t.TariffNameID = tn.TariffNameID
+INNER JOIN Parking.Zones AS z ON z.ZoneID = t.ZoneID
+INNER JOIN Parking.ZoneTypes AS zt ON zt.ZoneTypeID = z.ZoneTypeID
+INNER JOIN Parking.Lots AS l ON l.LotID = z.LotID
+INNER JOIN Location.Cities AS c ON c.CityID = l.CityID
+INNER JOIN Parking.SlotSizes AS s ON s.SlotSizeID = zt.SlotSizeID
+INNER JOIN Services.CalendarDates AS cd1 ON cd1.DateID = t.TariffStartDate
+INNER JOIN Services.CalendarDates AS cd2 ON cd2.DateID = t.TariffEndDate
+
+
+
+
