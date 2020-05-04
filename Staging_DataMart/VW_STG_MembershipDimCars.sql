8a0f51da3a9066f23706df57a7672b266de303f8
YukiAme <anyalo307@gmail.com>
2020-05-04 20:49:42 +0300

    Added Views for prepping staging DB tables Membership.DimClients & Membership.DimCars. Removed empty placeholder files

---
+++ Staging_DataMart/VW_STG_MembershipDimCars.sql
@@ -0,0 +1,11 @@
+/* Create OLTP view for future data transfer into Data Mart Membership dimension Cars */
+CREATE VIEW VW_STG_MembershipDimCars
+AS
+SELECT CarID
+    , Plate
+    , Brand
+	, Model
+    , ClientID
+FROM Clientele.Cars
+INNER JOIN Clientele.CarModels ON Clientele.Cars.CarModelID = Clientele.CarModels.CarModelID
+WHERE ClientID IS NOT NULL
