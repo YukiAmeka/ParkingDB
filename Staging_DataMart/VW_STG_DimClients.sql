e711a52b8c27c6a55a6ccba7dd144971a7ad6372
Vasyl Andrusiak <vasyl.andrusiak2017@gmail.com>
2020-05-06 23:03:05 +0300

    Changed view Clients

--- Staging_DataMart/VW_STG_DimClients.sql
+++ Staging_DataMart/VW_STG_DimClients.sql
@@ -1,7 +1,7 @@
 CREATE VIEW VW_STG_DimClients
 AS
 SELECT C.ClientID, C.FirstName AS [Name], C.Surname, C.Gender, C.Telephone, C.Email, C.HomeAddress, 
-C.IsCurrent, Ct.CityName AS CityWhereLive, Cit.CityName AS [ClosestCityWithParking]
+Ct.CityName, O.PurchaseDate, O.ExpiryDate
 FROM [Clientele].[Clients] AS C
 JOIN [Location].[Cities] AS Ct ON C.CityID = Ct.CityID
-JOIN [Location].[Cities] AS Cit ON Cit.CityID = Ct.ClosestCityWithParking
+JOIN [Membership].[Orders] AS O ON C.ClientID = O.ClientID
