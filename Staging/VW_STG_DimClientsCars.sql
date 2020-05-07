25e4842c81e5ca1f2b754e9380f9770fd6ba3f15
Vasyl Andrusiak <vasyl.andrusiak2017@gmail.com>
2020-05-07 11:41:17 +0300

    Added view for DimClientCars

---
+++ Staging/VW_STG_DimClientsCars.sql
@@ -0,0 +1,8 @@
+CREATE VIEW VW_STG_DimClientsCars
+AS
+SELECT C.ClientID, CL.FirstName, CL.Surname, CL.Gender, CL.Telephone, CT.CityName,
+CL.HomeAddress, CL.IsCurrent, C.Plate, CM.Brand, CM.Model, C.CarID
+FROM [Clientele].[Cars] AS C
+JOIN [Clientele].[Clients] AS CL ON C.ClientID = CL.ClientID
+JOIN [Clientele].[CarModels] AS CM ON C.CarModelID = CM.CarModelID
+JOIN [Location].[Cities] AS CT ON CL.CityID = CT.CityID
