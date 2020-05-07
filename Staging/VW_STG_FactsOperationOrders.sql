b9123ecb6914586b63c9d5c0ff9cb81615ea7bd7
StasGit <kutsyukk@gmail.com>
2020-05-07 13:43:01 +0300

    Changed VW_STG_FactsOperationOrders

--- Staging/VW_STG_FactsOperationOrders.sql
+++ Staging/VW_STG_FactsOperationOrders.sql
@@ -1,13 +1,15 @@
-CREATE VIEW  VW_STG_FactsOperationOrders
+CREATE VIEW  [dbo].[VW_STG_FactsOperationOrders2]
 AS
- SELECT o.OrderID, o.ZoneID, o.CarID, o.EmployeeOnEntry, o.EmployeeOnExit,
-		CAST(c.TheDate AS DATETIME) + CAST(o.TimeEntry AS DATETIME) AS DateTimeStart, 
-		CAST(c1.TheDate AS DATETIME)  + CAST(o.TimeExit AS DATETIME) AS DateTimeEnd, 
-		o.TotalCost, 	o.TariffID, o.AllCardID
- FROM Operation.Orders o
- JOIN Operation.Tariffs t ON o.TariffID = t.TariffID
- JOIN Services.CalendarDates c 
- ON o.DateEntry = c.DateID
- JOIN Services.CalendarDates c1
- ON o.DateExit = c1.DateID
+ SELECT  o.OrderID, o.ZoneID, o.CarID,
+		CAST(c.TheDate AS DATETIME) + CAST(o.TimeEntry AS DATETIME) AS DateTimeEntry, 
+		CAST(c1.TheDate AS DATETIME)  + CAST(o.TimeExit AS DATETIME) AS DateTimeExit, o.TotalCost, o.TariffID	
+		 FROM Operation.Orders o
+		 JOIN Operation.Tariffs t ON o.TariffID = t.TariffID
+		 JOIN Services.CalendarDates c 
+		 ON o.DateEntry = c.DateID
+		 JOIN Services.CalendarDates c1
+		 ON o.DateExit = c1.DateID
 
+	WHERE o.DateExit <= (SELECT MAX (DateExit) FROM Operation.Orders) -7  
+	ORDER BY DateTimeEntry
+	OFFSET 0 ROWS
