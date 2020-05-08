4a0a29b008bee6fbe7bc093440f07b7e95420250
Ihor <ihorprytula89@gmail.com>
2020-05-08 19:17:28 +0300

    change structure in view VW_STG_MembershipDimTariffs

--- Staging/VW_STG_MembershipDimTariffs.sql
+++ Staging/VW_STG_MembershipDimTariffs.sql
@@ -14,10 +14,10 @@ SELECT
 ,EndDate
 
 FROM Membership.Tariffs 
-INNER JOIN Parking.Zones ON Membership.Tariffs.ZoneID = Parking.Zones.ZoneID
-INNER JOIN Parking.ZoneTypes ON Parking.Zones.ZoneTypeID = Parking.ZoneTypes.ZoneTypeID
-INNER JOIN Parking.SlotSizes ON Parking.ZoneTypes.SlotSizeID = Parking.SlotSizes.SlotSizeID
-INNER JOIN Parking.Lots ON Parking.Zones.LotID = Parking.Lots.LotID
-INNER JOIN Location.Cities ON Parking.Lots.CityID = Location.Cities.CityID
+LEFT JOIN Parking.Zones ON Membership.Tariffs.ZoneID = Parking.Zones.ZoneID
+LEFT JOIN Parking.ZoneTypes ON Parking.Zones.ZoneTypeID = Parking.ZoneTypes.ZoneTypeID
+LEFT JOIN Parking.SlotSizes ON Parking.ZoneTypes.SlotSizeID = Parking.SlotSizes.SlotSizeID
+LEFT JOIN Parking.Lots ON Parking.Zones.LotID = Parking.Lots.LotID
+LEFT JOIN Location.Cities ON Parking.Lots.CityID = Location.Cities.CityID
 INNER JOIN Membership.Periods ON Membership.Tariffs.PeriodID = Membership.Periods.PeriodID
 
