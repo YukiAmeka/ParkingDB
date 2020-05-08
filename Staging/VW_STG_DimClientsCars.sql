834fcff8c9d449caafbac3c5cbfd11bf448ae035
Mariia-ter <terletska.mariia@gmail.com>
2020-05-09 00:59:25 +0300

    VW_STG_DimClientsCars is changed

--- Staging/VW_STG_DimClientsCars.sql
+++ Staging/VW_STG_DimClientsCars.sql
@@ -1,6 +1,6 @@
 CREATE VIEW VW_STG_DimClientsCars
 AS
-SELECT C.ClientID, CL.FirstName, CL.Surname, CL.Gender, CL.Telephone, CT.CityName,
+SELECT C.ClientID, CL.FirstName, CL.Surname, CL.Gender, CL.Telephone, CL.Email, CT.CityName,
 CL.HomeAddress, CL.IsCurrent, C.Plate, CM.Brand, CM.Model, C.CarID
 FROM [Clientele].[Cars] AS C
 JOIN [Clientele].[Clients] AS CL ON C.ClientID = CL.ClientID
