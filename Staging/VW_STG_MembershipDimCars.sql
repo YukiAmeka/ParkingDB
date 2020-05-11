28ea501d92d779047437c61f0b2c86776006592b
YukiAme <anyalo307@gmail.com>
2020-05-12 01:07:29 +0300

    Narrowed down MembershipDimCardsView

--- Staging/VW_STG_MembershipDimCars.sql
+++ Staging/VW_STG_MembershipDimCars.sql
@@ -8,4 +8,6 @@ SELECT CarID
     , ClientID
 FROM Clientele.Cars
 INNER JOIN Clientele.CarModels ON Clientele.Cars.CarModelID = Clientele.CarModels.CarModelID
-WHERE ClientID IS NOT NULL
+WHERE ClientID IS NOT NULL AND
+    ClientID IN
+        (SELECT ClientID FROM VW_STG_MembershipDimClients)
