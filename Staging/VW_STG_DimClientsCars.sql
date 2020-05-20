a4ade625c8fd49c27605de1fd42c06dc0e2cd2d4
Vasyl Andrusiak <vasyl.andrusiak2017@gmail.com>
2020-05-20 08:22:14 +0300

    Changed view

--- Staging/VW_STG_DimClientsCars.sql
+++ Staging/VW_STG_DimClientsCars.sql
@@ -1,8 +1,8 @@
 CREATE VIEW VW_STG_DimClientsCars
 AS
-SELECT C.ClientID, CL.FirstName, CL.Surname, CL.Gender, CL.Telephone, CL.Email, CT.CityName,
-CL.HomeAddress, CL.IsCurrent, C.Plate, CM.Brand, CM.Model, C.CarID
-FROM [Clientele].[Cars] AS C
-JOIN [Clientele].[Clients] AS CL ON C.ClientID = CL.ClientID
-JOIN [Clientele].[CarModels] AS CM ON C.CarModelID = CM.CarModelID
-JOIN [Location].[Cities] AS CT ON CL.CityID = CT.CityID
+SELECT Client.ClientID, Client.FirstName, Client.Surname, Client.Gender, Client.Telephone, Client.Email, 
+Loc.CityName, Client.HomeAddress, Client.IsCurrent, Cars.Plate, Model.Brand, Model.Model, Cars.CarID
+FROM [Clientele].[Cars] AS Cars
+INNER JOIN [Clientele].[CarModels] AS Model ON Cars.CarModelID = Model.CarModelID
+RIGHT JOIN [Clientele].[Clients] AS Client ON Cars.ClientID = Client.ClientID
+INNER JOIN [Location].[Cities] AS Loc ON Client.CityID = Loc.CityID
